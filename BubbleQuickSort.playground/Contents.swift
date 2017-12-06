93bae2c59bcc7af05c63b9e538c7e2e249bb457e
System Administrator <oalandgrave@gmail.com>
2017-12-06 15:57:28 -0500

    refactoring name to english

--- BubbleQuickSort.playground/Contents.swift
+++ BubbleQuickSort.playground/Contents.swift
@@ -2,44 +2,42 @@
 import UIKit
 
 
-func QuickSort(array arreglo:inout[Int], lowValue low:Int ,highValue high: Int){
+func QuickSort(array localArray:inout[Int], lowValue low:Int ,highValue high: Int){
     
     if (low < high) {
         
-        let p = partition(array: &arreglo, lowValue: low, highValue: high)
-        QuickSort(array: &arreglo, lowValue: low, highValue: p-1)
-        QuickSort(array: &arreglo, lowValue: p+1, highValue: high)
+        let p = partition(array: &localArray, lowValue: low, highValue: high)
+        QuickSort(array: &localArray, lowValue: low, highValue: p-1)
+        QuickSort(array: &localArray, lowValue: p+1, highValue: high)
         
     }
     
 }
 
-func partition(array arreglo:inout[Int], lowValue low:Int ,highValue high: Int) -> Int {
+func partition(array localArray:inout[Int], lowValue low:Int ,highValue high: Int) -> Int {
     
     var i = low-1
-    let toCompare = arreglo[high]
+    let toCompare = localArray[high]
     
     for j in low ... high-1{
         
-        if ( arreglo[j] <= toCompare ) {
+        if ( localArray[j] <= toCompare ) {
             i+=1
-            swipe(array: &arreglo, firstValue: i, secondValue: j)
+            swipe(array: &localArray, firstValue: i, secondValue: j)
         }
         
     }
-    swipe(array: &arreglo, firstValue: high, secondValue: i+1)
+    swipe(array: &localArray, firstValue: high, secondValue: i+1)
     return i+1
 }
 
-func swipe(array arreglo:inout[Int],firstValue i:Int , secondValue j:Int){
+func swipe(array localArray:inout[Int],firstValue i:Int , secondValue j:Int){
     
-    let pivote = arreglo[i]
-    arreglo[i] = arreglo[j]
-    arreglo[j] = pivote
+    let pivot = localArray[i]
+    localArray[i] = localArray[j]
+    localArray[j] = pivot
     
 }
-
-
 
 
 
